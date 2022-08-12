5f9cb1ad70febfa05dd442b1fe264a15d5cc2830
Ivan Pavlak <ivan.pavlak@gideonbros.ai>
2022-08-12 23:26:20 +0200

    Add control buttons to GUI

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ import threading
 import numpy as np
 import pandas as pd
 
-from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QStyle, QMainWindow, QApplication
 
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import matplotlib.animation as animation
 
 # App
 from visualizations import *
@@ -45,16 +44,63 @@ def indicator_func(data):
 #######################################
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
         # self.canvas = MplCanvas() -> maybe this is better, and I should send only Canvas to Visualization object ?
         self.vis = Visualization(data)
-        self.setCentralWidget(self.vis)
+        # self.setCentralWidget(self.vis)
 
         self.comm = Communicate()
         self.comm.update_vis_signal.connect(self.vis.animation.event_source.call)
+
+        self.sim = Simulator(self.comm, self.vis, 1.2)
+
+
+        widget = QWidget(self)
+        vlay = QVBoxLayout(widget)
+        hlay = QHBoxLayout()
+
+        play_btn = QPushButton()
+        pixmap = getattr(QStyle, 'SP_MediaPlay')
+        play_icon = self.style().standardIcon(pixmap)
+        play_btn.setIcon(play_icon)
+        play_btn.clicked.connect(self.sim.start)
+        hlay.addWidget(play_btn)
+
+        pause_btn = QPushButton()
+        pixmap = getattr(QStyle, 'SP_MediaPause')
+        pause_icon = self.style().standardIcon(pixmap)
+        pause_btn.setIcon(pause_icon)
+        pause_btn.clicked.connect(self.sim.pause)
+        hlay.addWidget(pause_btn)
+
+        stop_btn = QPushButton()
+        pixmap = getattr(QStyle, 'SP_MediaStop')
+        stop_icon = self.style().standardIcon(pixmap)
+        stop_btn.setIcon(stop_icon)
+        stop_btn.clicked.connect(self.sim.stop)
+        hlay.addWidget(stop_btn)
+
+        prev_btn = QPushButton()
+        pixmap = getattr(QStyle, 'SP_MediaSeekBackward')
+        prev_icon = self.style().standardIcon(pixmap)
+        prev_btn.setIcon(prev_icon)
+        # prev_btn.clicked.connect(self.sim.step_backward)
+        hlay.addWidget(prev_btn)
+
+        next_btn = QPushButton()
+        pixmap = getattr(QStyle, 'SP_MediaSeekForward')
+        next_icon = self.style().standardIcon(pixmap)
+        next_btn.setIcon(next_icon)
+        # next_btn.clicked.connect(self.sim.step_forward)
+        hlay.addWidget(next_btn)
+
+        vlay.addLayout(hlay)
+        vlay.addWidget(self.vis)
+        self.setCentralWidget(widget)
+        
 
         # myDataLoop = threading.Thread(name = 'myDataLoop', target = self.run, daemon = True, args=(self.vis.start_sim, self.vis.stop_sim))
         # myDataLoop.start()
@@ -83,12 +129,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.vis.stop_sim()
 
     def _run(self):
-        sim = Simulator(self.comm, self.vis, 1.2)
+        # sim = Simulator(self.comm, self.vis, 1.2)
         start_time = data.loc[data['Date'] >= '2019-1-3 00:00'].iloc[0].Date
         stop_time = data.loc[data['Date'] <= '2019-1-6 00:00'].iloc[-1].Date
-        sim.setup_simulator(data, start_time, stop_time, 1.2, True, tick_data)
+        self.sim.setup_simulator(data, start_time, stop_time, 0.4, False)
         # sim.add_indicator(indicator_func=indicator_func)
-        sim.start()
+        self.sim.start()
 
 ''' End Class'''
 
@@ -102,6 +148,6 @@ class Communicate(QtCore.QObject):
 ''' End Class '''
 
 
-app = QtWidgets.QApplication(sys.argv)
+app = QApplication(sys.argv)
 win = MainWindow()
 sys.exit(app.exec_())
