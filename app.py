0c4e09d779e684e457b4cefee7c796ce397505fa
Ivan Pavlak <ivan.pavlak@gideonbros.ai>
2023-09-12 07:28:33 +0200

    Refactor app.py

--- app.py
+++ app.py
@@ -25,65 +25,27 @@ tick_data_file = 'data/EURUSD/EURUSD1.csv'
 ########################################
 
 
-def connectSimAndUI(ui, sim):
-    ui.play_btn.clicked.connect(sim.start)
-    ui.pause_btn.clicked.connect(sim.pause)
-    ui.stop_btn.clicked.connect(sim.stop)
-    ui.prev_btn.clicked.connect(sim.step_backward)
-    ui.next_btn.clicked.connect(sim.step_forward)
+def testRun(start_vis, stop_vis):
+    sleep(1) # wait for first draw to init animation
 
-class MainWindow():
-    def __init__(self, main_window):
-
-        # self.canvas = MplCanvas() -> maybe this is better, and I should send only Canvas to Visualization object ?
-        self.vis = Visualization()
-        # self.setCentralWidget(self.vis)
-
-        self.comm = Communicate()
-        self.comm.update_vis_signal.connect(self.vis.animation.event_source.call)
-
-        self.sim = Simulator(self.comm, self.vis, 1.2)
-
-        ui = UI()
-        ui.setup_ui(main_window, self.vis)
-        connectSimAndUI(ui, self.sim)
+    frame_vis_event = threading.Event()
+    frame_vis_event.set()
+    
+    # comm.start_vis_signal.emit()
+    vis.start()
+    for i in range(8):
+        # print(i)
+        # self.vis.update_frame_idx(i)
+        frame_vis_event.wait()
+        comm.update_vis_signal.emit(frame_vis_event, i+10)
+        sleep(0.05) #TODO: check for 0.01 error
+    # comm.start_vis_signal.emit()
+    # for i in range(8):
+    #     self.vis.update_frame_idx(i)
+    #     sleep(0.5)
+    vis.stop()
 
 
-        # myDataLoop = threading.Thread(name = 'myDataLoop', target = self.run, daemon = True, args=(self.vis.start_sim, self.vis.stop_sim))
-        # myDataLoop.start()
-        self._run()
-
-        main_window.show()
-
-    def run(self, start_vis, stop_vis):
-        sleep(1) # wait for first draw to init animation
-
-        frame_vis_event = threading.Event()
-        frame_vis_event.set()
-        
-        # comm.start_vis_signal.emit()
-        self.vis.start()
-        for i in range(8):
-            # print(i)
-            # self.vis.update_frame_idx(i)
-            frame_vis_event.wait()
-            self.comm.update_vis_signal.emit(frame_vis_event, i+10)
-            sleep(0.05) #TODO: check for 0.01 error
-        # comm.start_vis_signal.emit()
-        # for i in range(8):
-        #     self.vis.update_frame_idx(i)
-        #     sleep(0.5)
-        self.vis.stop()
-
-    def _run(self):
-        start_time = pd.Timestamp('2019-1-3 00:00')
-        stop_time = pd.Timestamp('2019-1-6 00:00')
-        self.sim.setup_simulator(data_file, start_time, stop_time, interval=0.2, use_ticks=True, tick_data_file=tick_data_file)
-        self.sim.add_indicator(indicator_name='indicator_ex1', indicator='indicators/indicator_example1.py')
-        self.sim.add_trader(trader_name='trader_ex1', trader='traders/trader_example1.py')
-        # self.sim.start()
-
-''' End Class'''
 
 # You need to setup a signal slot mechanism, to
 # send data to your GUI in a thread-safe way.
@@ -95,6 +57,22 @@ class Communicate(QtCore.QObject):
 ''' End Class '''
 
 
+def connectSimAndUI(ui, sim):
+    ui.play_btn.clicked.connect(sim.start)
+    ui.pause_btn.clicked.connect(sim.pause)
+    ui.stop_btn.clicked.connect(sim.stop)
+    ui.prev_btn.clicked.connect(sim.step_backward)
+    ui.next_btn.clicked.connect(sim.step_forward)
+
+def runSim():
+    start_time = pd.Timestamp('2019-1-3 00:00')
+    stop_time = pd.Timestamp('2019-1-6 00:00')
+    sim.setup_simulator(data_file, start_time, stop_time, interval=0.2, use_ticks=False, tick_data_file=tick_data_file)
+    sim.add_indicator(indicator_name='indicator_ex1', indicator='indicators/indicator_example1.py')
+    sim.add_trader(trader_name='trader_ex1', trader='traders/trader_example1.py')
+    # sim.start()
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = QMainWindow()
@@ -104,6 +82,26 @@ if __name__ == '__main__':
     # print(g)
     # print(self.frameSize())
 
-    mw = MainWindow(win)
+
+    ### Visualization
+    # self.canvas = MplCanvas() -> maybe this is better, and I should send only Canvas to Visualization object ?
+    vis = Visualization()
+
+    ### Communication  (Slots and signals - Qt framework)
+    comm = Communicate()
+    comm.update_vis_signal.connect(vis.animation.event_source.call)
+
+    ### Simulator
+    sim = Simulator(comm, vis, 1.2)
+
+    ### User Interface
+    ui = UI()
+    ui.setup_ui(win, vis)
+    connectSimAndUI(ui, sim)
+
+
+    runSim()
+
+    win.show()
     
     sys.exit(app.exec_())
